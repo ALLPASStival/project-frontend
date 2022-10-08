9d6567215bd6568222ad08537af179fde86f3c8f
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2022-10-08 20:07:22 +0900

    Feat: 초기 세팅

---
+++ layouts/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Redirect, Route, Switch } from "react-router";
+import loadable from "@loadable/component";
+
+const Main = loadable(() => import(`@layouts/Main`));
+
+const App = () => {
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Redirect to="main" />
+      </Route>
+      <Route path="/main" component={Main} />
+    </Switch>
+  );
+};
+
+export default App;
