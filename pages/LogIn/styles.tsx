38174a3d355e95c953aa7084fe98d7d45e1d50da
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2022-10-13 19:52:22 +0900

    Delete styles.tsx

--- pages/LogIn/styles.tsx
+++
@@ -1,7 +0,0 @@
-import styled from "@emotion/styled";
-
-export const Wrapper = styled.div``;
-export const Header = styled.div``;
-export const Form = styled.div``;
-export const Lable = styled.div``;
-export const LoginBtn = styled.div``;
