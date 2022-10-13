e2900c764aab7fdd0680cc73c4cbbe3a0382a895
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2022-10-13 19:52:53 +0900

    Feat: 로그인, 회원가입, 마이페이지 화면만 구현

---
+++ pages/LogIn/styles.tsx
@@ -0,0 +1,7 @@
+import styled from "@emotion/styled";
+
+export const Wrapper = styled.div``;
+export const Header = styled.div``;
+export const Form = styled.div``;
+export const Lable = styled.div``;
+export const LoginBtn = styled.div``;
