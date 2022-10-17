8f2165d7e1ee547374ba51373bf19b0ab078c697
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2022-10-18 00:22:28 +0900

    Feat: 회원가입 페이지 구축 중

--- pages/SignUp/index.tsx
+++ pages/SignUp/index.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { Header, SubHeader, Wrapper } from "@pages/LogIn/styles";
+import { Link } from "react-router-dom";
 
 const SingUp = () => {
-  return <div>로그인 페이지</div>;
+  return (
+    <Wrapper>
+      <Header>ALLPASSTIVAL</Header>
+      <SubHeader>계정 만들기</SubHeader>
+    </Wrapper>
+  );
 };
 
 export default SingUp;
