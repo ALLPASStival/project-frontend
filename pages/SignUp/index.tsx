7e6b1c5bf1a841dd90e2e712cb778d9982952f56
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2022-10-18 12:23:26 +0900

    Feat: 회원가입 페이지구현중

--- pages/SignUp/index.tsx
+++ pages/SignUp/index.tsx
@@ -1,12 +1,98 @@
-import React from "react";
-import { Header, SubHeader, Wrapper } from "@pages/LogIn/styles";
-import { Link } from "react-router-dom";
+import React, { useCallback } from "react";
+import {
+  Header,
+  Input,
+  Lable,
+  SubHeader,
+  Wrapper,
+  Form,
+} from "@pages/LogIn/styles";
+import axios from "axios";
+import useInput from "@hooks/useInput";
+import { SignUpBtn, CheckBtn } from "@pages/SignUp/styles";
 
 const SingUp = () => {
+  const [id, onChangeId, setId] = useInput("");
+  const [nickname, onChangeNickname, setNickname] = useInput("");
+  const [password, onChangePassword, setPassword] = useInput("");
+  const [passwordcheck, onChangePasswordCheck, setPasswordCheck] = useInput("");
+
+  const onSubmit = useCallback(
+    (e: any) => {
+      e.preventDefault();
+      axios
+        .post(
+          "",
+          {
+            id,
+            password,
+          },
+          {}
+        )
+
+        .then((response) => {
+          alert("성공");
+        })
+        .catch((error) => {
+          alert("에러");
+        });
+    },
+    [id, password]
+  );
+
   return (
     <Wrapper>
       <Header>ALLPASSTIVAL</Header>
       <SubHeader>계정 만들기</SubHeader>
+      <Form onSubmit={onSubmit}>
+        <Lable>
+          <div>아이디</div>
+          <Input
+            type="text"
+            id="id"
+            name="id"
+            value={id}
+            onChange={onChangeId}
+            placeholder="아이디"
+          />
+          <CheckBtn onClick={onSubmit}>아이디 중복 확인</CheckBtn>
+        </Lable>
+        <Lable>
+          <div>닉네임</div>
+          <Input
+            type="text"
+            id="password"
+            name="password"
+            value={nickname}
+            onChange={onChangeNickname}
+            placeholder="닉네임"
+          />
+          <CheckBtn onClick={onSubmit}>닉네임 중복 확인</CheckBtn>
+        </Lable>
+        <Lable>
+          <div>비밀번호</div>
+          <Input
+            type="text"
+            id="password"
+            name="password"
+            value={password}
+            onChange={onChangePassword}
+            placeholder="비밀번호"
+          />
+        </Lable>
+        <Lable>
+          <div>비밀번호 확인</div>
+          <Input
+            type="text"
+            id="password"
+            name="password"
+            value={passwordcheck}
+            onChange={onChangePasswordCheck}
+            placeholder="비밀번호 확인"
+          />
+        </Lable>
+        <SignUpBtn onClick={onSubmit}>회원가입</SignUpBtn>
+      </Form>
     </Wrapper>
   );
 };
---
+++ pages/SignUp/styles.tsx
@@ -0,0 +1,39 @@
+import styled from "@emotion/styled";
+
+export const SignUpBtn = styled.div`
+  vertical-align: middle;
+  text-align: center;
+  width: 9rem;
+  height: 2.7rem;
+  background-color: #fe9010;
+  color: black;
+  border-radius: 25px;
+  outline: none;
+  font-size: 1.5rem;
+  font-weight: bold;
+  cursor: pointer;
+  margin-top: 1rem;
+  &:hover {
+    background-color: #f93011;
+    border: none;
+  }
+`;
+
+export const CheckBtn = styled.div`
+  vertical-align: middle;
+  text-align: center;
+  width: 6rem;
+  height: 1.7rem;
+  background-color: #fe9010;
+  color: black;
+  border-radius: 25px;
+  outline: none;
+  font-size: 0.5rem;
+  font-weight: bold;
+  cursor: pointer;
+  margin-top: 1rem;
+  &:hover {
+    background-color: #f93011;
+    border: none;
+  }
+`;
