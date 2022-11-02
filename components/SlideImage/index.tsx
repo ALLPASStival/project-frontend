25eab8f04da0fdafe1a13c5083a1ca4ef9a1e286
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2022-11-03 00:16:44 +0900

    Feat: 슬라이더 구현완료

---
+++ components/SlideImage/index.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import { slideList } from "@utils/slideList";
+import { SliderItem, Wrapper } from "@components/SlideImage/styles";
+
+const SliderImage = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 1000,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    centerMode: true,
+    centerPadding: "0px",
+  };
+
+  return (
+    <Wrapper>
+      <Slider {...settings} className={"slide"}>
+        {slideList.map((v, index) => (
+          <SliderItem key={index}>
+            <img src={v.src} alt={v.alt} />
+          </SliderItem>
+        ))}
+      </Slider>
+    </Wrapper>
+  );
+};
+
+export default SliderImage;
