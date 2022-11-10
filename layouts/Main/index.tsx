abd78b41c088ba7b97c5f988d9c9d33c124528d7
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2022-11-10 19:41:49 +0900

    Feat: 메인페이지에 FestivalRank 컴포넌트 추가

--- layouts/Main/index.tsx
+++ layouts/Main/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import HeaderBar from "@components/HeaderBar";
 import SliderImage from "@components/SlideImage";
+import FestivalRank from "@components/FestivalRank";
 
 const Main = () => {
   return (
     <div>
       <HeaderBar />
       <SliderImage />
+      <FestivalRank />
     </div>
   );
 };
