ee947a3228e38ed31ac548452147915facff3538
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2022-12-14 12:23:23 +0900

    Feat: 축제 찾기 페이지 구현 중

--- pages/Search/styles.tsx
+++ pages/Search/styles.tsx
@@ -1,10 +1,87 @@
 import styled from "@emotion/styled";
+import React from "react";
 
 export const SearchMain = styled.div`
   display: flex;
   flex-direction: column;
 `;
-export const TopHeader = styled.div``;
+export const TopHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  height: 4rem;
+`;
+export const TopTitle = styled.div`
+  font-size: 2rem;
+  font-weight: bold;
+`;
 
-export const MiddleSide = styled.div``;
+export const MiddleSide = styled.div`
+  display: flex;
+  justify-content: space-between;
+  gap: 1.5rem;
+  height: 4rem;
+  margin-bottom: 0.5rem;
+`;
+export const LeftOpt = styled.select`
+  width: 20rem;
+  height: 2.5rem;
+  background-color: #ffb763;
+  text-align-last: center;
+  text-align: center;
+  font-size: 1.2rem;
+  font-weight: bold;
+  border-radius: 5px;
+`;
+export const MiddleOpt = styled.select`
+  width: 20rem;
+  height: 2.5rem;
+  background-color: #ffb763;
+  text-align-last: center;
+  text-align: center;
+  font-size: 1.2rem;
+  font-weight: bold;
+  border-radius: 5px;
+`;
+export const RightOpt = styled.select`
+  width: 20rem;
+  height: 2.5rem;
+  background-color: #ffb763;
+  text-align-last: center;
+  text-align: center;
+  font-size: 1.2rem;
+  font-weight: bold;
+  border-radius: 5px;
+`;
+
 export const BottomSide = styled.div``;
+export const FestivalList = styled.div`
+  width: 100%;
+  height: 5rem;
+  background-color: #d9d9d9;
+  border-radius: 10px;
+  margin-bottom: 1rem;
+`;
+export const ListTop = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-left: 2rem;
+  margin-right: 2rem;
+  padding: 0.5rem;
+`;
+export const ListTopTitle = styled.div`
+  font-size: 1rem;
+  font-weight: bold;
+`;
+export const ListTopGood = styled.div``;
+export const ListBottom = styled.div``;
+
+export const TopSearch = styled.div`
+  width: 20rem;
+  height: 3rem;
+  background-color: #fe9010;
+  color: black;
+  border-radius: 25px;
+  border: none;
+  font-size: 1.5rem;
+  font-weight: bold;
+`;
