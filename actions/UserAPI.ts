aeff1ac293c97b06456cdb89d02b033f0b27090c
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2022-12-19 02:35:05 +0900

    Feat: API 연결

--- actions/UserAPI.ts
+++ actions/UserAPI.ts
@@ -1,5 +1,8 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import { User } from "../reducers/user";
 
+// 테스트 코드
 const delay = (time: number, value: object) =>
   new Promise((resolve, reject) => {
     setTimeout(() => {
@@ -7,8 +10,31 @@ const delay = (time: number, value: object) =>
     }, time);
   });
 
-export const LoginUser = createAsyncThunk("test/add", async () => {
+export const logIn = createAsyncThunk("user/logIn", async (data, thunkAPI) => {
+  // throw new Error('비밀번호가 틀렸습니다.');
   return await delay(500, {
-    value: 10,
+    userId: 1,
+    nickname: "seongjun",
   });
 });
+
+// export const addUserAsync = createAsyncThunk("ADD_USER", async (user: User) => {
+//     const response = await axios.post(
+//         "http://3.36.112.187:8080/api/v1/auth/register",
+//         user
+//     );
+//     alert("회원가입에 성공하였습니다.");
+//     return response.data;
+// });s
+
+//로그인
+// export const setUserAsync = createAsyncThunk(
+//     "SET_USER",
+//     async (user: User): Promise<User> => {
+//         const response = await axios.post(
+//             "http://localhost:3090/api/auth/login/${user.id}"
+//         );
+//         alert("로그인에 성공하였습니다.");
+//         return response.data;
+//     }
+// );
