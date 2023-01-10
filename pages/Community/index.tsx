0611d4a1a5a209a6c4cea4ede5c7b4478faa3691
Seryeong Kim <99seryoung50@gmail.com>
2023-01-10 15:19:12 +0900

    design: 헤더 추가

--- pages/Community/index.tsx
+++ pages/Community/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import HeaderBar from "@components/HeaderBar";
 import { Wrapper } from "../../Style/Wrapper";
 import {
@@ -7,20 +7,55 @@ import {
   SearchMain,
   TopHeader,
 } from "@pages/Search/styles";
+import { StyledDivCenter, StyledDivRow } from "../../Style/FlexBox";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { Category, SearchBox } from "./style";
 
 const Community = () => {
+  const [cat, setCat] = useState("커뮤니티");
+
   return (
     <>
       <HeaderBar />
       <Wrapper
         style={{
           height: "100%",
-          width: "90%",
-          paddingLeft: "5rem",
-          paddingRight: "5rem",
+          width: "100%",
         }}
       >
-        <div>커뮤니티</div>
+        <StyledDivRow
+          style={{
+            justifyContent: "space-between",
+            padding: "5rem 10rem 5rem 10rem"
+          }}
+        >
+          <span style={{ fontSize: "4.5rem", fontWeight: "bold" }}>{cat}</span>
+          <SearchBox>
+            <FontAwesomeIcon icon={faSearch} style={{ fontSize: "2.5rem" }} />
+            <input type="text" id="search" name="search" />
+          </SearchBox>
+        </StyledDivRow>
+        <StyledDivRow style={{ justifyContent: "space-evenly" }}>
+          <Category
+            onClick={() => setCat("커뮤니티")}
+            className={`${cat == "커뮤니티" ? "select" : ""}`}
+          >
+            자유게시판
+          </Category>
+          <Category
+            onClick={() => setCat("후기 게시판")}
+            className={`${cat == "후기 게시판" ? "select" : ""}`}
+          >
+            후기게시판
+          </Category>
+          <Category
+            onClick={() => setCat("구인 게시판")}
+            className={`${cat == "구인 게시판" ? "select" : ""}`}
+          >
+            구인게시판
+          </Category>
+        </StyledDivRow>
       </Wrapper>
     </>
   );
