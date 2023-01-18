9d9795c2fafc4354767907611c25e82cedaa7a25
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2023-01-18 20:04:56 +0900

    Feat: 개별 축제, 전체 축제 조회 api

--- actions/FestivalAPI.ts
+++ actions/FestivalAPI.ts
@@ -1,21 +1,37 @@
-// import { createAsyncThunk } from "@reduxjs/toolkit";
-// import axios from "axios";
-//
-// import { FestivalState } from "../reducers/festival";
-//
-// //축제정보 불러오기
-// export const getFestival = createAsyncThunk<FestivalState, any>(
-//   "GET_FESTIVAL",
-//   async (festival: FestivalState) => {
-//     try {
-//       const response = await axios.get(
-//         "http://3.36.112.187:8080/api/festivals/{축제id}"
-//       );
-//       console.log(response.data);
-//
-//       return response.data;
-//     } catch (e: any) {
-//       return e.rejectWithValue(e.response.data);
-//     }
-//   }
-// );
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+import { FestivalState } from "../reducers/festival";
+
+//축제정보 불러오기
+export const getFestival = createAsyncThunk<FestivalState, any>(
+  "GET_FESTIVAL",
+  async (festival: FestivalState) => {
+    try {
+      const response = await axios.get(
+        "http://3.36.112.187:8080/api/v1/festivals/1"
+      );
+      console.log(response.data);
+
+      return response.data;
+    } catch (e: any) {
+      return e.rejectWithValue(e.response.data);
+    }
+  }
+);
+
+export const getEachFestival = createAsyncThunk<FestivalState, any>(
+  "GET_FESTIVAL",
+  async (festival: FestivalState) => {
+    try {
+      const response = await axios.get(
+        "http://3.36.112.187:8080/api/v1/festivals/전체"
+      );
+      console.log(response.data);
+
+      return response.data;
+    } catch (e: any) {
+      return e.rejectWithValue(e.response.data);
+    }
+  }
+);
