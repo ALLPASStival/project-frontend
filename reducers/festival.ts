24f33bd5904a2ce6b2b9dc58a8acfa836967904f
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2023-01-18 20:04:37 +0900

    Feat: 축제 타입, 변수 이름 선언

--- reducers/festival.ts
+++ reducers/festival.ts
@@ -1,39 +1,61 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { RootState } from "../redux/store";
-// import { getFestival } from "../actions/FestivalAPI";
+import { getEachFestival, getFestival } from "../actions/FestivalAPI";
 //
-// export interface FestivalState {
-//   festival: {
-//     name: string;
-//     place: string;
-//     day: string;
-//     time: string;
-//     price: string;
-//     id: number;
-//   };
-//   error: any;
-// }
-//
-// const initialState: FestivalState = {
-//   festival: {
-//     name: "",
-//     place: "",
-//     day: "",
-//     time: "",
-//     price: "",
-//     id: 0,
-//   },
-//   error: "",
-// };
-//
-// export const festivalSLice = createSlice({
-//   name: "festival",
-//   initialState,
-//   reducers: {},
-//   extraReducers: (builder) =>
-//     builder.addCase(getFestival.fulfilled, (state, action) => {
-//       state.festival = action.payload.festival;
-//     }),
-// });
-//
-// export default festivalSLice;
+export interface FestivalState {
+  festival: object;
+  festivalId: string;
+  author: string;
+  etc: string;
+  festivalName: string;
+  startDate: string;
+  finishDate: string;
+  holdingVenue: string;
+  homepAddr: string;
+  hostInst: string;
+  hostOrg: string;
+  streetAddr: string;
+  telNum: string;
+
+  error: any;
+}
+
+const initialState: FestivalState = {
+  festival: {},
+  festivalId: "",
+  author: "",
+  etc: "",
+  festivalName: "",
+  startDate: "",
+  finishDate: "",
+  holdingVenue: "",
+  homepAddr: "",
+  hostInst: "",
+  hostOrg: "",
+  streetAddr: "",
+  telNum: "",
+
+  error: "",
+};
+
+export const festivalSLice = createSlice({
+  name: "festival",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) =>
+    builder
+      .addCase(getFestival.fulfilled, (state, action) => {
+        state.festival = action.payload.festival;
+      })
+      .addCase(getEachFestival.fulfilled, (state, action) => {
+        state.author = action.payload.author;
+        state.festivalName = action.payload.festivalName;
+        state.festivalId = action.payload.festivalId;
+        state.startDate = action.payload.startDate;
+        state.finishDate = action.payload.finishDate;
+        state.streetAddr = action.payload.streetAddr;
+        state.telNum = action.payload.telNum;
+      }),
+});
+
+export default festivalSLice;
