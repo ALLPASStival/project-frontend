eb54c04c5f1bb609580c09c402b7d133b878a38a
Seryeong Kim <99seryoung50@gmail.com>
2023-01-19 01:30:49 +0900

    design: 커뮤니티 글쓰기 레이아웃

--- pages/WritingCommunity/index.tsx
+++ pages/WritingCommunity/index.tsx
@@ -1,12 +1,40 @@
 import React, { useEffect } from "react";
 import HeaderBar from "@components/HeaderBar";
 import { Wrapper } from "../../Style/Wrapper";
+import {
+  Block,
+  Category,
+  Left,
+  OrgBtn,
+  Right,
+  StyledDiv,
+  Table,
+} from "../../Style/Community";
 
 const WritingCommunity = () => {
   return (
     <>
       <HeaderBar />
-      <Wrapper></Wrapper>
+      <Wrapper style={{ width: "100%" }}>
+        <Category>글쓰기</Category>
+        <Table>
+          <Block>
+            <Left>제목</Left>
+            <Right>제목 넣는 input</Right>
+          </Block>
+          <Block>
+            <Left>작성자</Left>
+            <Right>작성자 받아 와서 여기 넣기!</Right>
+          </Block>
+          <Block style={{ height: "21.3rem" }}>
+            <Left>본문</Left>
+            <Right>본문 넣는 input</Right>
+          </Block>
+        </Table>
+        <StyledDiv>
+          <OrgBtn>글 작성하기</OrgBtn>
+        </StyledDiv>
+      </Wrapper>
     </>
   );
 };
