2622b99854073ac49b623a0dad4741915a970ff1
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2023-01-21 02:31:07 +0900

    Feat: 구인 게시판 구현중

---
+++ pages/WritingRecruit/index.tsx
@@ -0,0 +1,130 @@
+import React, { useCallback, useEffect } from "react";
+import HeaderBar from "@components/HeaderBar";
+import { Wrapper } from "../../Style/Wrapper";
+import {
+  Block,
+  Left,
+  OrgBtn,
+  BlockInPut,
+  StyledDiv,
+  CommunityContainer,
+  Form,
+} from "../WritingCommunity/styles";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { useAppDispatch } from "../../redux/hooks";
+import useInput from "@hooks/useInput";
+import { Category, Right, SearchBox } from "../../Style/Community";
+import { postRecruit } from "../../actions/Community";
+
+//22 구인
+const WritingRecruit = () => {
+  const dispatch = useAppDispatch();
+
+  const [articleContent, onChangeArticleContent, setArticleContent] =
+    useInput("");
+  const [title, onChangeTitle, setTitle] = useInput("");
+  const [writer, onChangeWriter, setwriter] = useInput("");
+
+  const onSubmitRecruit = useCallback(
+    (e: any) => {
+      e.preventDefault();
+
+      dispatch(postRecruit({ articleContent, title })).catch((error) => {
+        alert(error.err);
+      });
+    },
+    [articleContent, title]
+  );
+  //
+  // useEffect(() => {
+  //   dispatch(getRecruit({}))
+  //     .unwrap()
+  //     .then((response) => {
+  //       console.log("### response: ", response);
+  //     })
+  //     .catch((error) => {
+  //       console.log("### error: ", error);
+  //     });
+  // }, []);
+
+  return (
+    <>
+      <HeaderBar />
+      <Wrapper style={{ width: "100%" }}>
+        <Category>구인 쓰기</Category>
+        <Form onSubmit={onSubmitRecruit}>
+          <CommunityContainer>
+            <Block>
+              <Left>제목</Left>
+              <BlockInPut
+                type="text"
+                id="id"
+                name="id"
+                value={title}
+                onChange={onChangeTitle}
+                placeholder="제목을 입력해주세요"
+              />
+            </Block>
+            <Block>
+              <Left>작성자</Left>
+              <BlockInPut
+                type="text"
+                id="id"
+                name="id"
+                value={writer}
+                onChange={onChangeWriter}
+                placeholder="작성자"
+              />
+            </Block>
+            <Block>
+              <Left>축제</Left>
+              <Right>
+                {/*<SearchBox style={{ width: "26.7rem", height: "4.8rem" }}>*/}
+                {/*  <FontAwesomeIcon*/}
+                {/*    icon={faSearch}*/}
+                {/*    style={{ fontSize: "2.5rem" }}*/}
+                {/*  />*/}
+                {/*  <input*/}
+                {/*    type="text"*/}
+                {/*    id="search"*/}
+                {/*    name="search"*/}
+                {/*    style={{ width: "16rem", height: "4.5rem" }}*/}
+                {/*  />*/}
+                {/*</SearchBox>*/}
+              </Right>
+            </Block>
+            <Block>
+              <Left>성별</Left>
+              <Right>성별 받아 와서 여기 넣기!</Right>
+            </Block>
+            <Block>
+              <Left>나이</Left>
+              <Right>나이 받아 와서 여기 넣기!</Right>
+            </Block>
+            <Block>
+              <Left>별점</Left>
+              <Right></Right>
+            </Block>
+            <Block style={{ height: "21.3rem" }}>
+              <Left>본문</Left>
+              <BlockInPut
+                type="text"
+                id="id"
+                name="id"
+                value={articleContent}
+                onChange={onChangeArticleContent}
+                placeholder="본문을 입력해주세요"
+              />
+            </Block>
+          </CommunityContainer>
+          <StyledDiv>
+            <OrgBtn>글 작성하기</OrgBtn>
+          </StyledDiv>
+        </Form>
+      </Wrapper>
+    </>
+  );
+};
+
+export default WritingRecruit;
