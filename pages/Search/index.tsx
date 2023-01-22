fb11521514dc4c3e29b9cd54696c6e5765f13e09
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2023-01-22 14:02:45 +0900

    Chore: 필요없는 부분 삭제

--- pages/Search/index.tsx
+++ pages/Search/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import HeaderBar from "@components/HeaderBar";
 import { Wrapper } from "../../Style/Wrapper";
-// import ReactSearchBox from "react-search-box";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faMagnifyingGlass,
@@ -28,7 +28,6 @@ import {
   TopTitle,
 } from "@pages/Search/styles";
 import FestivalReviews from "@components/FestivalReviews";
-import { SearchBox } from "@pages/Community/style";
 import { StyledDivRow } from "../../Style/FlexBox";
 
 const Search = () => {
