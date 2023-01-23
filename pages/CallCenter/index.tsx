40535a5c344edf693e33375b1594743f8ea43861
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2023-01-23 23:50:45 +0900

    Feat: 고객센터 홈페이지 개설

--- pages/CallCenter/index.tsx
+++ pages/CallCenter/index.tsx
@@ -1,18 +1,61 @@
-import React from "react";
-import { useAppDispatch, useAppSelector } from "../../redux/hooks";
-import { resetValue, testCount } from "../../reducers/TestSlice";
-import { addValue } from "../../actions/TestAPI";
+import React, { useState } from "react";
+import HeaderBar from "@components/HeaderBar";
+import { Wrapper } from "../../Style/Wrapper";
+import { StyledDivRow } from "../../Style/FlexBox";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { Category } from "./styles";
+import { OrgBtn, SearchBox, StyledDiv } from "../../Style/Community";
+
+import GetCommunity from "@components/GetCommunity";
+import { TopSearch } from "@pages/Search/styles";
+import GetReview from "@components/GetReview";
+import GetRecruit from "@components/GetRecruit";
+import { Link } from "react-router-dom";
+import GetCallCenter from "@components/GetCallCenter";
 
 const CallCenter = () => {
-  const value = useAppSelector(testCount);
-  const dispatch = useAppDispatch();
+  const [category, setCategory] = useState("자유 게시판");
+  // const ClickCategoryBtn = (e: any) => {
+  //   const { category } = e.target;
+  //   setCategory(category);
+  // };
+  const selectComponent: any = {
+    "자유 게시판": <GetCommunity />,
+    "후기 게시판": <GetReview />,
+    "구인 게시판": <GetRecruit />,
+  };
 
   return (
-    <div>
-      콜센터
-      <button onClick={() => dispatch(addValue())}>10증가</button>
-      <button onClick={() => dispatch(resetValue())}>리셋</button>
-    </div>
+    <>
+      <HeaderBar />
+      <Wrapper
+        style={{
+          height: "100%",
+          width: "100%",
+          justifyContent: "flex-start",
+        }}
+      >
+        <StyledDivRow
+          style={{
+            justifyContent: "space-between",
+            padding: "5rem 10rem 5rem 10rem",
+          }}
+        >
+          <span style={{ fontSize: "4.5rem", fontWeight: "bold" }}>
+            고객 센터
+          </span>
+          <TopSearch>
+            <FontAwesomeIcon icon={faSearch} style={{ fontSize: "2.5rem" }} />
+            <input type="text" id="search" name="search" />
+          </TopSearch>
+        </StyledDivRow>
+        <StyledDivRow style={{ justifyContent: "space-evenly" }}>
+          <Category>FAQ</Category>
+        </StyledDivRow>
+        <GetCallCenter />
+      </Wrapper>
+    </>
   );
 };
 
---
+++ pages/CallCenter/styles.tsx
@@ -0,0 +1,14 @@
+import styled from "@emotion/styled";
+import { StyledDivCenter } from "../../Style/FlexBox";
+
+export const Category = styled(StyledDivCenter)`
+  background-color: #ffdd95;
+  font-size: 3.2rem;
+  font-weight: bold;
+  flex: 1;
+  height: 6.6rem;
+
+  &.select {
+    background-color: #fe9010;
+  }
+`;
