f14adc79562c1e516fe7310fe4adf3beccfad774
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2023-01-23 13:43:03 +0900

    Feat: 지도 정보 불러오기

--- pages/Map/index.tsx
+++ pages/Map/index.tsx
@@ -3,6 +3,8 @@ import HeaderBar from "@components/HeaderBar";
 import { Wrapper } from "../../Style/Wrapper";
 import { getMap } from "../../actions/Map";
 import { useAppDispatch } from "../../redux/hooks";
+import { useSelector } from "react-redux";
+import { RootState } from "../../redux/store";
 
 declare global {
   interface Window {
@@ -26,9 +28,11 @@ const Map = () => {
       });
   }, []);
 
-  console.log(getMap);
-  console.log(getMap);
-  console.log(getMap);
+  const Map = useSelector((state: RootState) => state.map.content);
+
+  console.log(Map[0]);
+  console.log(Map[1]);
+  console.log(Map.length);
 
   useEffect(() => {
     const mapContainer = document.getElementById("map"); //지도를 담을 영역의 DOM 레퍼런스
@@ -50,83 +54,6 @@ const Map = () => {
     // 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
     const zoomControl = new kakao.maps.ZoomControl();
     map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
-
-    // 마커를 표시할 위치와 title 객체 배열입니다
-    // const positions = [
-    //   {
-    //     content:
-    //       '<div class="customoverlay">' +
-    //       '  <a href="https://map.kakao.com/link/map/11394059" target="_blank">' +
-    //       '    <span class="title">구의야구공원</span>' +
-    //       "  </a>" +
-    //       "</div>",
-    //     latlng: new kakao.maps.LatLng(37.31594605446268, 126.83897034588333),
-    //   },
-    //   {
-    //     content: "<div>고잔역</div>",
-    //     latlng: new kakao.maps.LatLng(37.31692035756915, 126.82398648462613),
-    //   },
-    //   {
-    //     content: "<div>한대앞역</div>",
-    //     latlng: new kakao.maps.LatLng(37.31021215429533, 126.85375998182458),
-    //   },
-    //   {
-    //     content: "<div>고잔고</div>",
-    //     latlng: new kakao.maps.LatLng(37.308620366341245, 126.83548910067815),
-    //   },
-    //   {
-    //     content: "<div>에리카</div>",
-    //     latlng: new kakao.maps.LatLng(37.29818781711024, 126.83652690419692),
-    //   },
-    // ];
-    //
-    // // 마커 이미지의 이미지 주소입니다
-    // const imageSrc =
-    //   "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
-    //
-    // for (let i = 0; i < positions.length; i++) {
-    //   // 마커 이미지의 이미지 크기 입니다
-    //   const imageSize = new kakao.maps.Size(24, 35);
-    //
-    //   // 마커 이미지를 생성합니다
-    //   const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
-    //
-    //   // 마커를 생성합니다
-    //   const marker = new kakao.maps.Marker({
-    //     map: map, // 마커를 표시할 지도
-    //     position: positions[i].latlng, // 마커를 표시할 위치
-    //     image: markerImage, // 마커 이미지
-    //   });
-    //
-    //   // 마커에 표시할 인포윈도우를 생성합니다
-    //   const infowindow = new kakao.maps.InfoWindow({
-    //     content: positions[i].content, // 인포윈도우에 표시할 내용
-    //   });
-    //
-    //   kakao.maps.event.addListener(
-    //     marker,
-    //     "mouseover",
-    //     makeOverListener(map, marker, infowindow)
-    //   );
-    //   kakao.maps.event.addListener(
-    //     marker,
-    //     "mouseout",
-    //     makeOutListener(infowindow)
-    //   );
-    // }
-    //
-    // function makeOverListener(map: any, marker: any, infowindow: any) {
-    //   return function () {
-    //     infowindow.open(map, marker);
-    //   };
-    // }
-    //
-    // // 인포윈도우를 닫는 클로저를 만드는 함수입니다
-    // function makeOutListener(infowindow: any) {
-    //   return function () {
-    //     infowindow.close();
-    //   };
-    // }
 
     const marker = new kakao.maps.Marker({
       map: map,
@@ -159,6 +86,7 @@ const Map = () => {
 
     // 마커 위에 커스텀오버레이를 표시합니다
     // 마커를 중심으로 커스텀 오버레이를 표시하기위해 CSS를 이용해 위치를 설정했습니다
+
     const overlay = new kakao.maps.CustomOverlay({
       content: content,
       map: map,
