2eda19a6478c3ecc97e052abb3fff04edb90a5c7
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2023-01-24 19:56:50 +0900

    Chore: 로그아웃 추가

--- actions/UserAPI.ts
+++ actions/UserAPI.ts
@@ -61,11 +61,38 @@ export const getUserInfo = createAsyncThunk<InfoAtt, any>(
   "users/getUserInfo",
   async () => {
     try {
-      const res = await axios.get("http://3.36.112.187:8080/api/v1" + "/users", TokenHeader);
+      const res = await axios.get(
+        "http://3.36.112.187:8080/api/v1" + "/users",
+        TokenHeader
+      );
       console.log("❕유저 정보 조회❕ ", res.data);
       return res.data;
     } catch (e) {
       console.error("⚠️ 유저 정보 조회 ⚠️ ", e);
+    }
+  }
+);
+
+export const logoutUserAsync = createAsyncThunk<Register, any>(
+  "LOGOUT_USER",
+  async ({ jwt }: any, thunkAPI) => {
+    try {
+      const response = await axios.post(
+        "http://3.36.112.187:8080/api/v1/auth/logout",
+        { jwt },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("jwt")}`,
+          },
+        }
+      );
+      alert("로그아웃에 성공하였습니다.");
+      localStorage.removeItem("jwt");
+      return response.data;
+    } catch (e: any) {
+      alert("로그아웃에 실패하였습니다.");
+      console.log(e);
+      return thunkAPI.rejectWithValue(e.response.data);
     }
   }
 );
